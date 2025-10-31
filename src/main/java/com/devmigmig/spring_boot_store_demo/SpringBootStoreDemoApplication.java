86fc839ee9bb000367ed1bd998bfdebde003fcbb
devmigmig <roymiguelcrucillo@gmail.com>
2025-10-31 15:18:45 +0800

    chore(init): initialize Spring Boot Store Demo project

---
+++ src/main/java/com/devmigmig/spring_boot_store_demo/SpringBootStoreDemoApplication.java
@@ -0,0 +1,13 @@
+package com.devmigmig.spring_boot_store_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStoreDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStoreDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmigmig/spring_boot_store_demo/SpringBootStoreDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmigmig.spring_boot_store_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStoreDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
